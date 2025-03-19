383f4d69abaecad15e0e73c8bd9c7caa6a59dc23
Akshu09 <akshayatakhandate09@gmail.com>
2025-03-19 22:24:41 +0530

    Added js files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+/*
+1. In javascript
+
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+
+// code readability should be high
+
+let name="akshu"
+let age=18
+let isLoggedIn=false
+
+// false => 2 to power 53
+//bigInt => 
+//string =>"" 
+//boolean =>  true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
